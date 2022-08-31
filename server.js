3a7caf665244e5a47d279359b496dc343ba2eb30
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-31 19:25:46 -0400

    able to pull up game through public folder on localhost

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const path = require("path");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  savUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now Listening"));
+});
