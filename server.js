88381b8eb413b6d95c92c081bce61549bde778f9
Andrew Baxter <asbaxter1@gmail.com>
2022-09-03 19:00:31 -0400

    slight change to import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const session = require("express-session");
-const routes = require("./controllers");
+const routes = require("./controllers/api");
 const path = require("path");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
